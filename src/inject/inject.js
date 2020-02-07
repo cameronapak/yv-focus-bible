6dc136d7b03e073317008e0a5858726fdd2e2a38
Cameron Pak <cameron.pak@life.church>
2020-02-06 22:58:32 -0600

    rm cmtry link temp & logged in focus reader

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,9 +3,11 @@
   - The purpose of this application is to toggle the bible focus mode on Bible.com.
 
   Author: Cameron Pak
-	TODO: Plan focus mode.
-	[X] Logged in focus mode.
-	TODO: Logged in highlights!
+  
+  * [X] Logged in focus mode.
+  * [ ] Figure out how to re-run on page change.
+	* [ ] Plan focus mode.
+	* [ ] Fix broken highlighting in focus mode.
 
 	Update: I turned off the logged in mode and plans for right now.
 */
@@ -19,23 +21,20 @@ var fileref // reference for injected css
 var ticking = false // for scrolling
 var scriptureMode = true // we assume scripture is open
 var planMode = false // if in a plan, then display plan focus
+var commentaryLink = '' // testing commentary link
 
 let isLoggedIn = false
 const checkLoginStatus = () => {
   const currentUrl = document.URL
   let loggedInUrl = 'my.bible.com'
 
-  if (currentUrl.includes(loggedInUrl)) {
+  if (currentUrl.toLowerCase().includes(loggedInUrl)) {
     isLoggedIn = true
   }
 }
 
-/* Testing Bible Commentary */
-var commentaryLink = ''
-
 // makes the verse in the middle of the screen focused
 function scrollCheck(e) {
-
   if (!isLoggedIn) {
     // make verses visible in the middle of screen
     getElementsInArea(e, {
@@ -60,6 +59,7 @@ function scrollCheck(e) {
 
   /* When user is logged in, code changes a little */
   if (isLoggedIn) {
+    console.log('I am logged in. Use different code for reader.')
     getElementsInArea(e, {
       elements: document.querySelectorAll('.bible-reader .book .verse'),
       markedClass: 'focus',
@@ -112,64 +112,63 @@ function enableFocusOnScroll() {
 }
 
 // toggle the focus reader on and off
-function toggleFocusReader(isEnabled) {
+function toggleFocusReader() {
+  console.log("Focus Bible: focus enabled.")
 
-  if (isEnabled == 'true') {
-    console.log("Focus Bible: focus enabled.")
+  // remove the header and Footer
+  removeHeaderAndFooter()
 
-    // remove the header and Footer
-    removeHeaderAndFooter()
+  // inject focus bible CSS
+  fileref = document.createElement("link")
+  fileref.setAttribute("rel", "stylesheet")
+  fileref.setAttribute("type", "text/css")
+  fileref.setAttribute("href", "chrome-extension://ioflbgigemihmblihppmhdnaefdaligj/src/inject/inject.css")
 
-    // inject focus bible CSS
-    fileref = document.createElement("link")
-    fileref.setAttribute("rel", "stylesheet")
-    fileref.setAttribute("type", "text/css")
-    fileref.setAttribute("href", "chrome-extension://gnknniobgdhfgiminhjehejgkkeghoka/src/inject/inject.css")
+  // fileref.setAttribute("href", "chrome-extension://jbjgbfheajcenmdgohnodpnigokdpdik/src/inject/inject.css")
+  document.getElementsByTagName("head")[0].appendChild(fileref)
 
-    // fileref.setAttribute("href", "chrome-extension://jbjgbfheajcenmdgohnodpnigokdpdik/src/inject/inject.css")
-    document.getElementsByTagName("head")[0].appendChild(fileref)
-
-    // add breathing circle and animated text
+  // add breathing circle and animated text
+  // TODO - uncomment later. This is disabled
+  if (false && commentaryLink) {
     reader.innerHTML = '<a id="commentary-link" href=""" target="_blank" style="text-decoration: none"><h4 class="mb5 tc f6 normal pa3 ba b--black-20 br2 outline-0 mid-gray bg-white ma0" style="padding: 16px">Bible Commentary</h4></a><div style="margin-bottom:15vh"><div class="circle"></div><div class="start-ch"> Breathe and relax as you begin to spend time with God. </div><div id="read-prompt" class="ready"> Scroll when ready. </div></div>' + reader.innerHTML
-
     // add commentary link
     document.getElementById('commentary-link').setAttribute('href', commentaryLink)
-
-    // set read prompt
-    // document.getElementById('read-prompt').innerText = "HEY!!"
-
-    // grabs all elemens in the area
-    getElementsInArea = (function (docElm) {
-      var viewportHeight = docElm.clientHeight
-
-      return function (e, opts) {
-        var found = [],
-          i
-
-        if (e && e.type == 'resize')
-          viewportHeight = docElm.clientHeight
-
-        for (i = opts.elements.length i--) {
-          var elm = opts.elements[i],
-            pos = elm.getBoundingClientRect(),
-            topPerc = pos.top / viewportHeight * 100,
-            bottomPerc = pos.bottom / viewportHeight * 100,
-            middle = (topPerc + bottomPerc) / 2,
-            inViewport = middle > opts.zone[1] &&
-              middle < (100 - opts.zone[1])
-
-          elm.classList.toggle(opts.markedClass, inViewport)
-
-          if (inViewport)
-            found.push(elm)
-        }
-      }
-    })(document.documentElement)
-
-    enableFocusOnScroll()
-  } else if (isEnabled == 'false') {
-    console.log("Focus Bible: focus disabled.")
   }
+
+  reader.innerHTML = '<div style="margin: 15vh 0;"><div class="circle"></div><div class="start-ch"> Breathe and relax as you begin to spend time with God. </div><div id="read-prompt" class="ready"> Scroll when ready. </div></div>' + reader.innerHTML
+
+  // set read prompt
+  // document.getElementById('read-prompt').innerText = "HEY!!"
+
+  // grabs all elemens in the area
+  getElementsInArea = (function (docElm) {
+    var viewportHeight = docElm.clientHeight
+
+    return function (e, opts) {
+      var found = [],
+        i
+
+      if (e && e.type == 'resize')
+        viewportHeight = docElm.clientHeight
+
+      for (i = opts.elements.length; i--;) {
+        var elm = opts.elements[i],
+          pos = elm.getBoundingClientRect(),
+          topPerc = pos.top / viewportHeight * 100,
+          bottomPerc = pos.bottom / viewportHeight * 100,
+          middle = (topPerc + bottomPerc) / 2,
+          inViewport = middle > opts.zone[1] &&
+            middle < (100 - opts.zone[1])
+
+        elm.classList.toggle(opts.markedClass, inViewport)
+
+        if (inViewport)
+          found.push(elm)
+      }
+    }
+  })(document.documentElement)
+
+  enableFocusOnScroll()
 } /* on reading viewer */
 
 /* gets commentary for each book */
@@ -215,30 +214,17 @@ function getCommentaryLink() {
 function prepFocusMode() {
   // grab the Bible reader
   try {
-    reader = document.getElementsByClassName('yv-bible-text')[0]
-
-    if (reader === undefined) {
-      // // maybe the user is logged in, so try another
-      // reader = document.getElementsByClassName('bible-reader')[0]
-      // if (reader === undefined) {
-      // 	scriptureMode = false
-      // } else {
-      // 	loggedIn = true
-      // }
-
-      scriptureMode = false
+    if (!isLoggedIn) {
+      reader = document.getElementsByClassName('yv-bible-text')[0]
     } else {
-      getCommentaryLink()
-    }
-
-    // // maybe it's a bible plan
-    if (reader === undefined) {
-      // just try to see if its a bible plan
-      reader = document.getElementsByClassName('devo-content')[0]
-      if (reader !== undefined) {
-        planMode = true
-        console.log("Plan mode")
-        console.log(reader)
+      reader = document.getElementsByClassName('bible-reader')[0]
+      // // maybe it's a bible plan
+      if (reader === undefined) {
+        // just try to see if its a bible plan
+        reader = document.getElementsByClassName('devo-content')[0]
+        if (reader !== undefined) {
+          planMode = true
+        }
       }
     }
   } catch (error) {
@@ -264,7 +250,12 @@ function prepFocusMode() {
         isFocusEnabled = items.bibleFocusEnabled
 
         // start the focus reader
-        toggleFocusReader(isFocusEnabled)
+        if (isFocusEnabled) {
+          toggleFocusReader(isFocusEnabled)
+          console.log("Focus Bible: enabled.")
+        } else {
+          console.log("Focus Bible: focus disabled.")
+        }
       }
     })
   }
@@ -272,12 +263,12 @@ function prepFocusMode() {
 
 /* removes header and footer */
 function removeHeaderAndFooter() {
-  // header is removed in CSS
+  if (!isLoggedIn) {
+    // header is removed in CSS
 
-  // remove footer (hacky way to do it)
-  var footer = document.getElementsByClassName('fixed-l left-0-l w-100-l bottom-0-l bg-white b--black-20 bt-l z-999')
-  for (var i = (footer.length - 1) i >= 0 i--) {
-    footer[i].parentNode.removeChild(footer[i])
+    // remove footer (hacky way to do it)
+    var footer = document.getElementsByClassName('fixed-l left-0-l w-100-l bottom-0-l bg-white b--black-20 bt-l z-999')[0]
+    footer.remove()
   }
 }
 
